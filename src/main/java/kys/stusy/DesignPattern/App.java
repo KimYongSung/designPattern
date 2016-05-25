bd524e15f36d5d6b7869d4134de7d27c9522910c
YongSungKim <dydtjd0245@gmail.com>
2016-05-25 22:52:10 +0900

    최초 프로젝트 생성

---
+++ src/main/java/kys/stusy/DesignPattern/App.java
@@ -0,0 +1,13 @@
+package kys.stusy.DesignPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
